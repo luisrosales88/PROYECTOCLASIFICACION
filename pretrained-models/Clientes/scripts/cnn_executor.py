b8790ce19d703dee6f066703e3e4d02758769e14
luisrosales88 <46075087+luisrosales88@users.noreply.github.com>
2019-07-06 01:56:28 -0500

    Update cnn_executor.py

--- pretrained-models/Clientes/scripts/cnn_executor.py
+++ pretrained-models/Clientes/scripts/cnn_executor.py
@@ -7,18 +7,18 @@
 # Cargando modelo de disco
 import tensorflow as tf
 from keras.models import model_from_json
-from keras.models import load_model
+#from keras.models import load_model
 import matplotlib.pyplot as plt
 from keras.optimizers import Adam
 
 def cargarModelo():
-    #json_file = open('../model/model.json', 'r')
-    #loaded_model_json = json_file.read()
-    #json_file.close()
-    #loaded_model = model_from_json(loaded_model_json)
-    #loaded_model.load_weights("../model/model.h5")
+    json_file = open('../model/model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("../model/model.h5")
 	# Cargar la RNA desde disco
-    loaded_model = load_model("../model/model.h5")
+    #loaded_model = load_model("../model/model.h5")
     #print("Cargando modelo desde el disco ...")
     #loaded_model.compile(optimizer=Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08,decay=0.0), loss='binary_crossentropy', metrics=['accuracy'])
     print("Modelo cargado de disco!")
